d13dcc91ddc4c13c0253ca576978bf2fed0b3f09
Victor <victormwendwa804@gmail.com>
2023-07-13 21:56:59 -0700

    ?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: 1
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	Return(1);
+	return (1);
 }
 
