e2257f642428eefe905abac1f712f4d53e1c23ac
Victor <victormwendwa804@gmail.com>
2023-07-18 12:16:21 -0700

    My main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Add prototypes for other functions here, if applicable */
+
+#endif /* MAIN_H */
+
