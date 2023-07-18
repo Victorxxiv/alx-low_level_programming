5b9dca7c55e58ff805accf1ba5099149bfe206d3
Victor <victormwendwa804@gmail.com>
2023-07-18 13:03:27 -0700

    My 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,6 +7,7 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int i;
+	
 	for (i = 0; i < 10; i++)
 	{
 		/* Loop through the lowercase alphabet */
@@ -14,6 +15,7 @@ void print_alphabet_x10(void)
 		{
 			_putchar(letter); /* Print each lowercase letter */
 		}
+		
 		_putchar('\n'); /* Move to the next line after printing the alphabet */
 	}
 }
