90f3a39a67667f3c1a56754b9c0845ea11ffd50c
Victor <victormwendwa804@gmail.com>
2023-07-24 00:52:06 -0700

    My 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,35 @@
-/* 11-print_to_98.c */
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line.
+ * @n: The starting number.
+ */
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n <= 98)
 	{
-		printf("%d, ", n);
-		if (n < 98)
+		int i;
+
+		for (i = n; i <= 98; i++)
 		{
-			n++;
-		}
-		else
-		{
-			n--;
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
-	printf("98\n");
+	else
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,6 +18,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int num1, int num2);
 void print_alphabet(void);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
 
