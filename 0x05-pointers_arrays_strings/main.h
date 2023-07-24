434093ebdda091e0d5a521f47ba757501aacd078
Victor <victormwendwa804@gmail.com>
2023-07-24 12:56:14 -0700

    My 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates the value
+ *               it points to to 98.
+ * @n: Pointer to the integer to be updated
+ */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
