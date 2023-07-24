44a6b824b64b8f03f484e2944c6696d7cbe37cf5
Victor <victormwendwa804@gmail.com>
2023-07-24 01:55:28 -0700

    My 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the first 98 Fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int prev = 1, curr = 2, next;
+	int count;
+
+	printf("%lu, %lu", prev, curr);
+
+	for (count = 3; count <= 98; count++)
+	{
+		next = prev + curr;
+		printf(", %lu", next);
+
+		prev = curr;
+		curr = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
