8cf2f5a2affbeeb27e25d0d2912bccd71ed8a693
Victor <victormwendwa804@gmail.com>
2023-07-24 02:09:20 -0700

    My 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-/**
- * main - Finds and prints the first 98 Fibonacci numbers.
- *
- * Return: Always 0.
- */
+/*Function to print the first 'n' Fibonacci numbers separated by commas*/
+void print_fibonacci(int n);
+
 int main(void)
 {
-	unsigned long int prev = 1, curr = 2, next;
-	int count;
-
-	printf("%lu, %lu", prev, curr);
-
-	for (count = 3; count <= 98; count++)
-	{
-		next = prev + curr;
-		printf(", %lu", next);
-
-		prev = curr;
-		curr = next;
-	}
-
+	/*Print the first 98 Fibonacci numbers*/
+	print_fibonacci(98);
 	printf("\n");
-
 	return (0);
+}
+
+/*Recursive function to calculate and print the Fibonacci numbers*/
+void print_fibonacci(int n)
+{
+	static unsigned long int a = 1, b = 2;
+
+	if (n == 0)
+		return;
+
+	printf("%lu", a);
+	if (n != 1)
+		printf(", ");
+
+	b = a + b;
+	a = b - a;
+
+	print_fibonacci(n - 1);
 }
 
