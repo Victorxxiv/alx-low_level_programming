310803aa4688ce5a7be64b85101cac21ccee1de5
Victor <victormwendwa804@gmail.com>
2023-07-28 12:36:59 -0700

    My 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,22 @@
  */
 int main(void)
 {
-	int a[5] = {98, 402, 198, 298, 102};
+	int n;
+	int a[5];
+	int *p;
 
-	int *p = a + 2;
-	/* Your code goes here */
-	/* Use pointer arithmetic to access the value at index 2 */
-	/* without directly using the variable a */
-	printf("a[2] = %d\n", *(p));
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
