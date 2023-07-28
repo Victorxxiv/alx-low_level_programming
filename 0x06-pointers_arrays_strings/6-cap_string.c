7371ac723151d4192bcc880035065da147a80364
Victor <victormwendwa804@gmail.com>
2023-07-28 09:23:06 -0700

    My 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,37 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The input string.
+ * cap_string - Capitalizes all words of a string
+ * @str: The input string to be capitalized
  *
- * Return: A pointer to the modified string.
+ * Return: A pointer to the capitalized string
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
+	char separators[] = " \t\n,;.!?\"(){}";
 
-	/* Capitalize the first character if it's a letter */
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
+	/* Check if the first character is a letter and capitalize it if necessary */
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
 
-	while (str[i]) != '\0')
+	/* Iterate through each character in the string */
+	for (i = 1; str[i] != '\0'; i++)
 	{
-		/* Capitalize the character after a separator */
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-			str[i] == ',' || str[i] == ';' || str[i] == ',' ||
-			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-			str[i] == '(' || str[i] == ')' || '{' || str[i] == '}')
+		int j;
+		/* Check if the current character is a separator */
+		for (j = 0; separators[j] != '\0'; j++)
 		{
-			i++;
-
-			/* Capitalize the character if it's a letter */
-			if (str[i + 1] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
+			/* If the current character is a separator, check the next character */
+			if (str[i] == separators[j])
+			{
+				/* If the next character is a letter, capitalize it */
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+				break;
+			}
 		}
-
-		i++;
+	}
 
 	return (str);
 }
