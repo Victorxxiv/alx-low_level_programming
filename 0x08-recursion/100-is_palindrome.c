b775a2af20f3cda778bb429d2f6a259e071097de
Victor <victormwendwa804@gmail.com>
2023-08-05 01:01:13 -0700

    My 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,55 +1,45 @@
 #include "main.h"
 
-/**
- * _strlen - Returns the length of a string.
- * @s: The string to measure.
- *
- * Return: The length of the string.
- */
-int _strlen(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen(s + 1));
-}
+/* Function prototype for check_palindrome */
+int check_palindrome(char *s, int start, int end);
 
 /**
- * is_palindrome_recursive - Checks if a string is a palindrome recursively.
- * @s: The string to check.
- * @start: The starting index of the current comparison.
- * @end: The ending index of the current comparison.
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The input string to be checked.
  *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int is_palindrome_recursive(char *s, int start, int end)
-{
-	/* Base case: if the indexes cross, the string is a palindrome */
-	if (start >= end)
-		return (1);
-
-	/* If characters at the current indexes don't match, it's not a palindrome */
-	if (s[start] != s[end])
-		return (0);
-
-	 /* Continue recursion with updated indexes */
-	return (is_palindrome_recursive(s, start + 1, end - 1));
-}
-
-/**
- * is_palindrome - Wrapper function to check if a string is a palindrome.
- * @s: The string to check.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: 1 if palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
-	int lenght = _strlen(s);
+	int len = 0;
 
-	 /* Empty string is a palindrome */
-	if (length == 0)
+	/* Calculate the length of the string */
+	while (s[len] != '\0')
+		len++;
+
+	/* Call the helper function to check for palindrome */
+	return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * check_palindrome - Recursive helper function to check palindrome.
+ * @s: The input string.
+ * @start: Starting index.
+ * @end: Ending index.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	/* Base case: If start index is greater than or equal to end index */
+	if (start >= end)
 		return (1);
 
-	 /* Start recursion with the string and its boundaries */
-	return (is_palindrome_recursive(s, 0, lenght - 1));
+	/* Compare characters at start and end indices */
+	if (s[start] != s[end])
+		return (0);
+
+	/* Recursively check the next characters */
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
