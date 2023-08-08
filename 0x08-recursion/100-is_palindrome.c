331565b6a2a6cc748846cd997966ff085a3ac114
Victor <victormwendwa804@gmail.com>
2023-08-08 01:21:41 -0700

    My 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,59 +1,36 @@
 #include "main.h"
 
 /**
- * is_palindrome - Check if a string is a palindrome
- * @s: The string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-	int len = 0;
-	
-	if (*s == '\0')
-		return (1);
-
-	while (s[len])
-		len++;
-
-	return (check_palindrome(s, len));
-}
-
-/**
- * check_palindrome - Recursive function to check if a string is a palindrome
- * @s: The string to check
- * @len: The length of the string
- * @start: The starting index
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int check_palindrome(char *s, int len, int start)
-{
-	if (len <= 1)
-		return (1);
-
-	if (s[start] != s[start + len - 1])
-		return (0);
-
-	return (check_palindrome(s + 1, len - 2, start + 1));
-}
-
-/**
- * is_palindrome - Check if a string is a palindrome
- * @s: The string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The input string to check.
+ * Return: 1 if string is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
-		return (1);
+	while (s[len] != '\0')
+		len++; /* Find the length of the string */
 
-	while (s[len])
-		len++;
+	return (check_palindrome(s, 0, len - 1));
+}
 
-	return (check_palindrome(s, len, 0));
+/**
+ * check_palindrome - Helper function to check if a string is a palindrome.
+ * @s: The input string.
+ * @start: The starting index.
+ * @end: The ending index.
+ * Return: 1 if string is a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1); /* Base case: reached middle, string is a palindrome */
+
+	if (s[start] != s[end])
+		return (0); /* Characters at start and end don't match, not a palindrome */
+
+	/* Recur with inner substring */
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,9 +12,9 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
 int _strlen(char *s);
-int check_palindrome(char *s, int len);
 int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+int check_palindrome(char *s, int start, int end);
 
 #endif /* MAIN_H */
