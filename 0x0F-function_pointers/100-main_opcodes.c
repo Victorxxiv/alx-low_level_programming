b62d799620ae4fecd7c008814fd9899ff1b4ffc1
Victor <victormwendwa804@gmail.com>
2023-08-16 12:09:00 -0700

    My 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
- *
- * Return: 0 (Success), 1 (Incorrect arguments), 2 (Negative byte count)
- */
+/* Function to print the opcodes of a given memory location */
+void print_opcodes(char *main_ptr, int num_bytes);
+
 int main(int argc, char *argv[])
 {
-	int i, byte_count;
-
 	/* Check if the correct number of arguments is provided */
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	/* Convert the byte count argument to an integer */
-	byte_count + atoi(argv[1]);
+	/* Convert the argument to an integer */
+	int num_bytes = atoi(argv[1]);
 
-	/* Check if the byte count is negative */
-	if (byte_count < 0)
+	/* Check for negative number of bytes */
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
+
+	/* Get a pointer to the main function */
+	char *main_ptr = (char *)main;
 
 	/* Print the opcodes of the main function */
-	for (i = 0; i < byte_count; i++)
-	{
-		printf("%02hhX", *((char *)main + i));
-		if (i != byte_count - 1)
-			printf(" ");
-	}
-
-	printf("\n");
+	print_opcodes(main_ptr, num_bytes);
 
 	return (0);
+}
+
+void print_opcodes(char *main_ptr, int num_bytes)
+{
+	int i;
+	for (i = 0; i < num_bytes; i++)
+	{
+		/* Print opcode in hexadecimal format with leading zeros */
+		printf("%02hhx", main_ptr[i]);
+        
+		/* Print space between opcodes (except for the last one) */
+		if (i != num_bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 }
 
