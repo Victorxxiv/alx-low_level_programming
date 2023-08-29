9dc9552d813c5978b4f84c5070fd11bceec22be0
Victor <victormwendwa804@gmail.com>
2023-08-29 09:15:28 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,13 @@
 
 int _putchar(char c);
 
-/* Structure for singly linked list */
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
 	int n;
