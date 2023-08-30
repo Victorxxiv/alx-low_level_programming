e99beaac76b65b0730c76ff36ad687b93a1665e3
Victor <victormwendwa804@gmail.com>
2023-08-29 23:26:09 -0700

    My 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,6 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 #endif /* LISTS_H */
