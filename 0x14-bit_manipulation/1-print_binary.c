b631eb38908455bd7731cb61aabcc387eff6e34c
Victor <victormwendwa804@gmail.com>
2023-09-01 10:31:44 -0700

    My 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,32 @@
 #include "main.h"
 
 /**
- * print_binary - Print the binary representation of a number
- * @n: The number to be printed in binary
- */
-
+* print_binary - Print the binary representation of a number
+* @n: The number to be printed in binary
+*/
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1; /* Initialize */
-	int bit_count = sizeof(unsigned long int) * 8;
-	int printed = 0;
-
-	/* Start from the leftmost bit (most significant bit) */
-	mask <<= (bit_count - 1);
+	int bit_count = sizeof(unsigned long int) * 8; /* Calculate number of bits */
+	int i;
+	int flag = 0; /* A flag to indicate when the first '1' is encountered */
 
 	if (n == 0)
 	{
-		/* Special case: if n is 0, print '0' and return */
-		_putchar('0');
+		_putchar('0'); /* Special case: if n is 0, print '0' and return */
 		return;
 	}
 
-	while (mask > 0)
+	for (i = bit_count - 1; i >= 0; i--)
 	{
-		if ((n & mask) == 0)
-			_putchar('0'); /* If the bit is 0, print '0' */
-		else
+		if ((n >> i) & 1) /* Check the i-th bit from the left */
 		{
-			_putchar('1'); /* If the bit is 1, print '1' */
-			printed = 1; /* Set a flag indicating we've printed at least one '1' */
+			_putchar('1');
+			flag = 1;
 		}
-		mask >>= 1; /* Shift the mask to the right to check the next bit */
+		else if (flag)
+		{
+			_putchar('0');
+		}
 	}
-
-	if (!printed)
-		_putchar('0'); /* If no '1' was printed (n was all zeros), print '0' */
 }
 
