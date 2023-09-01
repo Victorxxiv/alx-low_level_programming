4774d4f55797412d96b489e6b531e50caad9a66d
Victor <victormwendwa804@gmail.com>
2023-09-01 13:14:38 -0700

    My 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-* get_endianness - Check the endianness of the system
+* get_endianness - Checks the endianness of the system.
 *
-* Return: 0 if big-endian, 1 if little-endian
+* Return: 0 if big endian, 1 if little endian
 */
 int get_endianness(void)
 {
-	unsigned int x = 1;
-	char *c = (char *)&x;
+	unsigned int num = 1;
+	char *endianCheck = (char *)&num;
 
-	/* If the least significant byte is stored in the lowest, little-endian */
-	if (*c)
-		return (1); /* Little-endian */
-	else
-	return (0); /* Big-endian */
+	/* If the least significant byte is stored first (little endian) */
+	if (*endianCheck == 1)
+		return (1); /* Little Endian */
+
+	/* If not little endian, assume big endian */
+	return (0); /* Big Endian */
 }
 
