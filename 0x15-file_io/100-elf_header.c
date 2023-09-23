fbd64a58ba73591df6f0367532e81446cd1f1f5c
Victor <victormwendwa804@gmail.com>
2023-09-23 11:08:53 -0700

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,49 +23,83 @@ void print_elf_header(Elf64_Ehdr *header)
 {
 int i;
 printf("ELF Header:\n");
-printf("  Magic:   ");
+printf("Magic: ");
 for (i = 0; i < EI_NIDENT; i++)
 printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
 
-printf("  Class:                             %s\n",
+printf("Class: %s\n",
 header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
 
-printf("  Data:                              %s\n",
+printf("Data: %s\n",
 header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
 header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid");
 
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-printf("  OS/ABI:                            ");
+printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+printf("OS/ABI: ");
 switch (header->e_ident[EI_OSABI])
 {
-case ELFOSABI_SYSV:    printf("UNIX - System V\n"); break;
-case ELFOSABI_HPUX:    printf("HP-UX\n"); break;
-case ELFOSABI_NETBSD:  printf("NetBSD\n"); break;
-case ELFOSABI_LINUX:   printf("Linux\n"); break;
-case ELFOSABI_SOLARIS: printf("Solaris\n"); break;
-case ELFOSABI_AIX:     printf("AIX\n"); break;
-case ELFOSABI_IRIX:    printf("IRIX\n"); break;
-case ELFOSABI_FREEBSD: printf("FreeBSD\n"); break;
-case ELFOSABI_TRU64:   printf("TRU64 UNIX\n"); break;
-case ELFOSABI_MODESTO: printf("Novell Modesto\n"); break;
-case ELFOSABI_OPENBSD: printf("OpenBSD\n"); break;
-default:                printf("<unknown: %d>\n", header->e_ident[EI_OSABI]);
+case ELFOSABI_SYSV:
+	printf("UNIX - System V\n");
+	break;
+case ELFOSABI_HPUX:
+	printf("HP-UX\n");
+	break;
+case ELFOSABI_NETBSD:
+	printf("NetBSD\n");
+	break;
+case ELFOSABI_LINUX:
+	printf("Linux\n");
+	break;
+case ELFOSABI_SOLARIS:
+	printf("Solaris\n");
+	break;
+case ELFOSABI_AIX:
+	printf("AIX\n");
+	break;
+case ELFOSABI_IRIX:
+	printf("IRIX\n");
+	break;
+case ELFOSABI_FREEBSD:
+	printf("FreeBSD\n");
+	break;
+case ELFOSABI_TRU64:
+	printf("TRU64 UNIX\n");
+	break;
+case ELFOSABI_MODESTO:
+	printf("Novell Modesto\n");
+	break;
+case ELFOSABI_OPENBSD:
+	printf("OpenBSD\n");
+	break;
+default:
+	printf("<unknown: %d>\n", header->e_ident[EI_OSABI]);
 }
 
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
 
-printf("  Type:                              ");
+printf("Type: ");
 switch (header->e_type)
 {
-case ET_NONE:    printf("NONE (Unknown type)\n"); break;
-case ET_REL:     printf("REL (Relocatable file)\n"); break;
-case ET_EXEC:    printf("EXEC (Executable file)\n"); break;
-case ET_DYN:     printf("DYN (Shared object file)\n"); break;
-case ET_CORE:    printf("CORE (Core file)\n"); break;
-default:         printf("<unknown: %d>\n", header->e_type);
+case ET_NONE:
+	printf("NONE (Unknown type)\n");
+	break;
+case ET_REL:
+	printf("REL (Relocatable file)\n");
+	break;
+case ET_EXEC:
+	printf("EXEC (Executable file)\n");
+	break;
+case ET_DYN:
+	printf("DYN (Shared object file)\n");
+	break;
+case ET_CORE:
+	printf("CORE (Core file)\n");
+	break;
+default:
+	printf("<unknown: %d>\n", header->e_type);
 }
 
-printf("  Entry point address:               0x%lx\n", header->e_entry);
+printf("Entry point address: 0x%lx\n", header->e_entry);
 }
 
 int main(int argc, char *argv[])
