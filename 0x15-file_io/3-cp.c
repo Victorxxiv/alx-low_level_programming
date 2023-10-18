70459e5f3d4a5862836cf58598654232ac552126
Victor <victormwendwa804@gmail.com>
2023-10-18 08:07:14 -0700

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,36 +7,46 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * exit_with_error - Exits the program with an error message.
+ *
+ * @message: The error message to display.
+ * @exit_code: The exit code to use.
+ */
+void exit_with_error(const char *message, int exit_code)
+{
+	dprintf(2, "%s\n", message);
+	exit(exit_code);
+}
+
+/**
  * main - Copies the content of one file to another.
- * @ac: Argument count.
- * @av: Argument vector.
+ *
+ * @argc: The argument count.
+ * @argv: The argument vector.
  * Return: 0 on success, or the appropriate exit code on failure.
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 int file_from, file_to;
 ssize_t bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
 
-if (ac != 3)
+if (argc != 3)
 {
-dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-exit(97);
+exit_with_error("Usage: cp file_from file_to", 97);
 }
 
-file_from = open(av[1], O_RDONLY);
+file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
-exit(98);
+exit_with_error("Error: Can't read from file", 98);
 }
 
-file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (file_to == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", av[2]);
-close(file_from);
-exit(99);
+close(file_from); /* Close the source file */
+exit_with_error("Error: Can't write to file", 99);
 }
 
 while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
@@ -44,30 +54,22 @@ while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 bytes_written = write(file_to, buffer, bytes_read);
 if (bytes_written == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", av[2]);
 close(file_from);
 close(file_to);
-exit(99);
+exit_with_error("Error: Can't write to file", 99);
 }
 }
 
 if (bytes_read == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
 close(file_from);
 close(file_to);
-exit(98);
+exit_with_error("Error: Can't read from file", 98);
 }
 
-if (close(file_from) == -1)
+if (close(file_from) == -1 || close(file_to) == -1)
 {
-dprintf(2, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-if (close(file_to) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", file_to);
-exit(100);
+exit_with_error("Error: Can't close file descriptor", 100);
 }
 
 return (0);
