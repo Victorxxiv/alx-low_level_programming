832ebf812daa743afc4755dee86ed4a78a64e17a
Victor <victormwendwa804@gmail.com>
2023-11-14 16:01:54 -0800

    Dynamic library

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Compile all .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+
+# Create the dynamic library
+gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
+
