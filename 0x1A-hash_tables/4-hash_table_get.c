4e538cb83d6fcb9f2133dca9775f86467f48e2ed
Victor <victormwendwa804@gmail.com>
2023-12-28 23:51:52 -0800

    Retrieves function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ *
+ * @ht: The hash table to look into.
+ * @key: The keey to look for.
+ *
+ * Return: Value associated with the key, or NULL if key is not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		/* If ht is NULL, key is NULL or empty string, return NULL. */
+		return NULL;
+	}
+
+	/* Hash the key */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* Search Linked List */
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		/* Compare the key of the node with the provided key. */
+		if (strcmp(current->key, key) == 0)
+		{
+			/* If a match is found, return the associated value. */
+			return current->value;
+		}
+		current = current->next;
+	}
+
+	/* If the key is not found in the linked list, return NULL. */
+	return NULL;
+}
+
